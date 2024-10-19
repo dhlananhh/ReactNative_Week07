22f047a264d7c67b0ab78049b9a1d9e75f064fe7
dhlananhh <21087481.anh@student.iuh.edu.vn>
2024-10-19 22:39:14 +0700

    Tạo giao diện App.js

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Screen01 from "./screens/Screen01"
+import Screen02 from "./screens/Screen02"
+import Screen03 from "./screens/Screen03"
+
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen01">
+        <Stack.Screen name="Screen01" component={Screen01} options={{ headerShown: false }} />
+        <Stack.Screen name="Screen02" component={Screen02} options={{ headerShown: false }} />
+        <Stack.Screen name="Screen03" component={Screen03} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
